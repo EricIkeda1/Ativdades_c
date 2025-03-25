50c18c6b7bd4378bb78c204085f47ca9dcb5f289
EricIkeda1 <93358246+EricIkeda1@users.noreply.github.com>
2025-03-25 20:48:02 -0300

    Add files via upload

---
+++ Atividade_1.c
@@ -0,0 +1,12 @@
+//Algoritmo para calcular a temperatura em graus celsius e aparesentar em graus fahrenheit
+#include<stdio.h>
+
+int main(){
+    float celsius, fahrenheit;
+    printf("Digite a temperatura em graus Celsius");
+    scanf("%f", &celsius),
+    fahrenheit = (1.8*celsius)+32;
+    printf("A temperatura em Fahrenheit: %.2fF\n", fahrenheit);
+
+    return 0;
+}
